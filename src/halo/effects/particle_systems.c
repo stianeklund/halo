ba30c739bbd9066d09dfb6016f6f1b29bdfcaedd
Stian Eklund <stian.eklund@gmail.com>
2026-04-13 08:45:27 +0200

    Implement particle_systems_dispose_from_old_map, particle_systems.c now 4/5
    Add particle_system_delete to kb.json for the dispose loop.

--- src/halo/effects/particle_systems.c
+++ src/halo/effects/particle_systems.c
@@ -15,3 +15,20 @@ void particle_systems_initialize_for_new_map(void)
 void particle_systems_dispose(void)
 {
 }
+
+void particle_systems_dispose_from_old_map(void)
+{
+  int particle_system_index;
+
+  if (particle_system_header_data && particle_system_header_data->valid) {
+    for (particle_system_index =
+           data_next_index(particle_system_header_data, NONE);
+         particle_system_index != NONE;
+         particle_system_index = data_next_index(particle_system_header_data,
+                                                 particle_system_index)) {
+      particle_system_delete(particle_system_index);
+    }
+    data_make_invalid(particle_system_header_data);
+    data_make_invalid(particle_system_data);
+  }
+}
