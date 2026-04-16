c54f1ea300b06ba72aa9098af7372426f5f16819
Stian Eklund <stian.eklund@gmail.com>
2026-04-16 14:53:31 +0200

    Add build date to UI widget text to match original style

--- tools/gen-build-info.py
+++ tools/gen-build-info.py
@@ -12,9 +12,10 @@ def main():
 	print(f'''
 #define BUILD_REV "{git_rev}"
 #define BUILD_DATE "{datetime.now(timezone.utc).isoformat()}"
+#define BUILD_DATE_SHORT "{datetime.now(timezone.utc).strftime('%b %d %Y')}"
 const char *build_rev = BUILD_REV;
 const char *build_date = BUILD_DATE;
-const char *build_ui_widget_text = "HDP " BUILD_REV;
+const char *build_ui_widget_text = "HDP " BUILD_REV " " BUILD_DATE_SHORT;
 ''')
 
 if __name__ == '__main__':
