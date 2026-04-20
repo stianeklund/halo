a7594bd898215e45d6c6b6d18e8a28cb9a864c26
Stian Eklund <stian.eklund@gmail.com>
2026-04-20 13:48:47 +0200

    tools: make the generated build info use the current time not utc
    makes it easier to reason about when the build was actually made:P

--- tools/gen-build-info.py
+++ tools/gen-build-info.py
@@ -1,13 +1,14 @@
-from datetime import datetime, timezone
+from datetime import datetime
 
 from build_hash import git_rev as _git_rev
 
 def main():
 	git_rev = _git_rev()
+	now = datetime.now()
 	print(f'''
 #define BUILD_REV "{git_rev}"
-#define BUILD_DATE "{datetime.now(timezone.utc).isoformat()}"
-#define BUILD_DATE_SHORT "{datetime.now(timezone.utc).strftime('%m/%d/%y')}"
+#define BUILD_DATE "{now.isoformat()}"
+#define BUILD_DATE_SHORT "{now.strftime('%m/%d/%y')}"
 const char *build_rev = BUILD_REV;
 const char *build_date = BUILD_DATE;
 const char *build_ui_widget_text = BUILD_REV " " BUILD_DATE_SHORT;
