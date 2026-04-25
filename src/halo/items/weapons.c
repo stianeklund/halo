2768d4bb5ea722578cec9e139ce534560b7aa0fb
Stian Eklund <stian.eklund@gmail.com>
2026-04-25 15:03:51 +0200

    ABI audit: 2 function(s) passed Port weapon_try_place
    Functions ported:
    - FUN_0009c750 @ 0x9c750 (?)
    - weapon_has_activity @ 0xfb3c0 (?)
    
    Coverage: 76.7% (1000/1303 symbols)

--- src/halo/items/weapons.c
+++ src/halo/items/weapons.c
@@ -16,3 +16,32 @@ void weapon_activate(int weapon_handle)
   int16_t frame = weapon_get_animation_frame(weapon_handle, 0, 10, -1);
   *(int16_t *)((int)weapon_data + 0x1ea) = frame;
 }
+
+/* Attempts to place (holster/put-away) the current weapon. If flag is
+ * zero and the weapon has active triggers or animations, the placement
+ * is rejected. On success, sets the put-away animation (state 10),
+ * resets trigger/magazine state, disposes any attached effect, and
+ * starts the put-away effect sequence. */
+bool weapon_try_place(int weapon_handle, int flag)
+{
+  uint32_t *weapon_data =
+    (uint32_t *)object_get_and_verify_type(weapon_handle, 4);
+  tag_get(0x77656170, weapon_data[0]);
+
+  if ((char)flag == 0 && weapon_has_activity(weapon_handle))
+    return false;
+
+  if (!weapon_set_animation_state(weapon_handle, flag, 10))
+    return false;
+
+  *(int16_t *)((int)weapon_data + 0x1e0) = 0;
+  weapon_reset_state(weapon_handle);
+
+  if (*(int *)((int)weapon_data + 0x274) != -1) {
+    FUN_0009c750(*(int *)((int)weapon_data + 0x274));
+    *(int *)((int)weapon_data + 0x274) = -1;
+  }
+
+  FUN_000de3b0(weapon_handle, 0xb);
+  return true;
+}
