88ae135b5e12edf67c57d45f2917070bddb76a5e
Stian Eklund <stian.eklund@gmail.com>
2026-04-25 15:08:52 +0200

    ABI audit: 2 function(s) passed Port edit_text_set_cursor_to_end
    Functions ported:
    - unicode_snap_cursor @ 0x19d300 (?)
    - edit_text_clamp_cursor @ 0x972b0 (?)
    
    Coverage: 76.6% (1000/1305 symbols)

---
+++ src/halo/dialogs/edit_text.c
@@ -0,0 +1,21 @@
+/* Moves the text cursor to the end of the edit text buffer and
+ * clears any active selection. Asserts that the edit_text struct
+ * is valid (non-null, has buffer, max_length > 0, strlen <= max). */
+void edit_text_set_cursor_to_end(void *edit_text)
+{
+  int *et = (int *)edit_text;
+
+  if (et == NULL || et[0] == 0 || *(int16_t *)((int)et + 4) <= 0 ||
+      (unsigned int)csstrlen((const char *)et[0]) >
+        (unsigned int)(int)*(int16_t *)((int)et + 4)) {
+    display_assert("valid_edit_text(edit)",
+                   "c:\\halo\\SOURCE\\dialogs\\edit_text.c", 0x9f, 1);
+    system_exit(-1);
+  }
+
+  edit_text_clamp_cursor(edit_text);
+
+  int16_t len = (int16_t)csstrlen((const char *)et[0]);
+  *(int16_t *)((int)et + 6) = len;
+  *(int16_t *)((int)et + 8) = -1;
+}
