0b9e39948c1c0021244fbb4e361c510b67298337
Stian Eklund <stian.eklund@gmail.com>
2026-04-26 08:08:04 +0200

    Port FUN_000211f0 actor variant tag lookup
    kb_meta.json updates:
    - 1 symbol(s) updated
    
    Coverage: 77.4% (1035/1338 symbols)

--- src/halo/common.c
+++ src/halo/common.c
@@ -73,6 +73,21 @@ char *FUN_000210f0(int actor_handle)
   return 0;
 }
 
+char *FUN_000211f0(int actor_handle)
+{
+  char *actor = (char *)datum_get(*(data_t **)0x6325a4, actor_handle);
+  char *actv = (char *)tag_get(0x61637476, *(int *)(actor + 0x5c));
+  int weapon_handle = FUN_0003b270(actor_handle);
+  if (weapon_handle != -1) {
+    int *obj = (int *)object_get_and_verify_type(weapon_handle, 4);
+    char *weap = (char *)tag_get(0x77656170, *obj);
+    if (weap != 0 && *(int *)(weap + 0x3c8) != -1) {
+      actv = (char *)tag_get(0x61637476, *(int *)(weap + 0x3c8));
+    }
+  }
+  return actv;
+}
+
 void vector3d_add(float *a, float *b, float *out)
 {
   out[0] = a[0] + b[0];
