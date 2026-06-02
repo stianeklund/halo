3689b163242942f92be7010182a84a704a5ca03e
Stian Eklund <stian.eklund@gmail.com>
2026-06-02 10:58:25 +0200

    add common.h

---
+++ common.h
@@ -0,0 +1,66 @@
+//
+// This header is included by default in all source files.
+//
+#ifndef COMMON_H
+#define COMMON_H
+
+#ifdef MSVC
+#pragma runtime_checks("scu", off)
+#endif
+
+#define DECOMP_CUSTOM 1 // Logic that is added to aid decompilation, etc
+#define DEBUG_BUILD 1 // Logic that appears only in debug builds
+
+extern const char *build_rev;
+extern const char *build_date;
+extern const char *build_ui_widget_text;
+
+float __cdecl sinf(float);
+float __cdecl cosf(float);
+float __cdecl sqrtf(float);
+float __cdecl fabsf(float);
+double __cdecl sin(double);
+double __cdecl cos(double);
+double __cdecl sqrt(double);
+double __cdecl fabs(double);
+
+#include "types.h"
+#define XBOX_REPLACE_STANDARD_NAMES
+#include "inlines.h"
+#include "decl.h"
+
+#define assert_halt(cond)                                    \
+    do {                                                     \
+        if (!(cond)) {                                       \
+            display_assert(#cond, __FILE__, __LINE__, true); \
+            system_exit(-1);                                 \
+        }                                                    \
+    } while (0)
+
+#define assert_halt_msg(cond, msg)                         \
+    do {                                                   \
+        if (!(cond)) {                                     \
+            display_assert(msg, __FILE__, __LINE__, true); \
+            system_exit(-1);                               \
+        }                                                  \
+    } while (0)
+
+#define CLAMP(x, low, high) \
+  ((x) < (low) ? (low) : ((x) > (high) ? (high) : (x)))
+
+#define MAXIMUM_GAMEPADS 4
+#define MAXIMUM_NUMBER_OF_LOCAL_PLAYERS 4
+#define MAXIMUM_STRING_SIZE            0x2000
+#define MAXIMUM_MEMSET_SIZE            0x10000000
+#define MAXIMUM_MEMCPY_MEMMOVE_SIZE    0x10000000
+#define PAGE_READWRITE                 0x04
+#define TICKS_PER_SECOND               (*(float *)0x253394) /* 30.0f */
+
+static const int _scenario_type_main_menu = 2;
+
+#ifdef DEBUG_BUILD
+#undef strlen
+#define strlen csstrlen
+#endif
+
+#endif
