170d6b8483dd79fb9851d3b0924acc21f2ebf633
Stian Eklund <stian.eklund@gmail.com>
2026-07-14 18:20:34 +0200

    Port FUN_000bb1c0 (players.obj) (97.0% VC71, 100/100 equiv)
    Functions ported:
    - FUN_000bb1c0 @ 0xbb1c0 (players.obj) [97.0% VC71, 17/16 insns]
    
    Coverage: 30.4% (2476/8142 symbols)

--- src/halo/game/players.c
+++ src/halo/game/players.c
@@ -780,6 +780,26 @@ void player_set_unit_camo_flag(int player_handle /* @<eax> */,
   }
 }
 
+/* Set a unit object flag bit (0x20) at unit+0x1b4 for a player's unit.
+ *
+ * Sibling of player_set_unit_camo_flag (0xbb180); another powerup branch of
+ * player_set_respawn_timer.  Looks up the player datum, fetches its unit
+ * object handle (player+0x34) and verifies it is a unit (object type mask 3,
+ * biped/vehicle family).  When param2 is 0, ORs bit 0x20 into the unit flags
+ * at +0x1b4.  object_get_and_verify_type is called unconditionally, before the
+ * branch, matching the original. */
+void FUN_000bb1c0(int player_index /* @<eax> */, int16_t param2)
+{
+  char *player;
+  char *unit_obj;
+
+  player = (char *)datum_get(player_data, player_index);
+  unit_obj = (char *)object_get_and_verify_type(*(int *)(player + 0x34), 3);
+  if (param2 == 0) {
+    *(unsigned int *)(unit_obj + 0x1b4) |= 0x20;
+  }
+}
+
 /* Allocate and initialise a new player datum.
  *
  * local_player_index  (a1) -- which local player slot to assign; NONE (-1) is
